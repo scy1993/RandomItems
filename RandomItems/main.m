f3e33f5659fc78eb9f3822e785e0e65240e589c8
jit <juicyiter@qq.com>
2018-10-09 13:34:59 +0800

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by JuicyITer on 09/10/2018.
+//  Copyright © 2018 JuicyITer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
